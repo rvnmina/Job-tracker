03cc5f5b2acf5d0bd9dc17220400eb2d1beafd4b
RAVINDRA MINA <rvn17mina@gmail.com>
2024-11-08 22:59:50 +0530

    initial commit

---
+++ client/src/components/Logo.js
@@ -0,0 +1,16 @@
+import logo from '../assets/images/finalLogo.png'
+
+const Logo = () => {
+  return (
+    <img
+      src={logo}
+      alt="JobTrackr"
+      padding-top="55px"
+      height="155px"
+      width="255px"
+      className="logo"
+    />
+  );
+}
+
+export default Logo
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,39 @@
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import { useAppContext } from '../context/appContext';
+import React from 'react';
+
+const Landing = () => {
+  const { user } = useAppContext();
+  return (
+    <React.Fragment>
+      {user && <Navigate to='/' />}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className='container page'>
+          {/* info */}
+          <div className='info'>
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>
+              
+Introducing a powerful job application and interview tracking website, designed to streamline your job search process. Stay organized, never miss an opportunity, and effortlessly manage your progress with ease. Boost your chances of success and land your dream job with our intuitive platform.
+            </p>
+            <Link to='/register' className='btn btn-hero'>
+              Login/Register
+            </Link>
+          </div>
+          <img src={main} alt='job hunt' className='img main-img' />
+        </div>
+      </Wrapper>
+    </React.Fragment>
+  );
+};
+
+export default Landing;
